36a3deb0c663476776726a2a50b14425d7c1267b
Kacper Garbolinski <s10632kg@ms.wwsi.edu.pl>
2025-02-16 11:18:35 +0100

    init

--- JIPP/JIPP/Program.cs
+++ JIPP/JIPP/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//zad 2
+Console.WriteLine("Podaj swoje imię:");
+var name = Console.ReadLine();
+Console.WriteLine("Hello " + name);
+//zad 3
+int result = 5 + 9;
